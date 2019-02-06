5ccd715a0a227e21c9fbd84f072f5446f237b8e9
Monika Gharat <monika.2006.it@gmail.com>
2019-02-06 19:20:47 +0530

    fill form, submit and  on submit clear form

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
-$(".list-group-item").click(function() {
-    $(".student-info").hide();
-    $("body").css("overflow", "hidden");
-  });
-  $(".add-new").click(function() {
-    $(".student-info").show();
-    $("body").css("overflow", "hidden");
-  });
-  
-  
+$(".list").click(function() {
+  $(".add-new").removeClass("active");
+  $(".list").addClass("active");
+  $(".student-info").hide();
+  $(".table").show();
+});
+
+$(".add-new").click(function() {
+  $(".list").removeClass("active");
+  $(".add-new").addClass("active");
+  $(".student-info").show();
+  $(".table").hide();
+});
+
+$(".student-info").submit(function(e) {
+  e.preventDefault(); /*stop reloding page*/
+  var allok = true;
+  var name = $(".js-form-name").val();
+  var emailid = $(".js-form-email").val();
+  var phoneno = $(".js-form-phone-no").val();
+  $(".js-form-name-alert").hide();
+  $(".js-form-email-alert").hide();
+  $(".js-form-phone-no-alert").hide();
+  if (name == "") {
+    $(".js-form-name-alert").show();
+    allok = false; /*if condition is false*/
+  }
+  if (emailid == "") {
+    $(".js-form-email-alert").show();
+    allok = false;
+  }
+  if (phoneno == "") {
+    $(".js-form-phone-no-alert").show();
+    allok = false;
+  }
+
+  if (allok == true) {
+    var arrStudentInfo = JSON.parse(localStorage.getItem("studentInfo")) || [];
+    arrStudentInfo.push({
+      name: name,
+      emailid: emailid,
+      phoneno: phoneno
+    });
+    localStorage.setItem("studentInfo", JSON.stringify(arrStudentInfo));
+    $(".student-info").trigger("reset");
+    $(".list").trigger("click");
+  }
+});
